8be347513ff5de859ccf0d87d8bb945eced640f2
Aada Nwajei <aada.nwajei@gmail.com>
2024-11-04 14:49:56 +0200

    injektoi StatisticsService-oliolle PlayerReader-luokan olio

--- viikko1/nhl-statistics-1/src/index.py
+++ viikko1/nhl-statistics-1/src/index.py
@@ -2,7 +2,9 @@ from statistics_service import StatisticsService
 
 
 def main():
-    stats = StatisticsService()
+    stats = StatisticsService(
+        PlayerReader("https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt")
+    )
     philadelphia_flyers_players = stats.team("PHI")
     top_scorers = stats.top(10)
 
