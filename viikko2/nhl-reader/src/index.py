28982af90537917ca42ce3ea445dd088018e3e21
Aada Nwajei <aada.nwajei@gmail.com>
2024-11-11 13:56:33 +0200

    Tehtävä 10

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,32 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = {}
+
+    for player_dict in response:
+        country = player_dict['nationality']
+        if country not in players:
+            players[country] = ""
+
+    for country in players:
+        countrys_players = []
+        for player_dict in response:
+            if player_dict['nationality'] == country:
+                countrys_players.append(Player(player_dict))
+        players[country] = countrys_players
+
+    for country in players:
+        print("\n"+"Players from "+country+"\n")
+        for player in players[country]:
+            print(player) 
+
 
 if __name__ == "__main__":
     main()
