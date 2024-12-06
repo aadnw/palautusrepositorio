80f748fb8bd96d40b4f5dadff0982481a5587f10
Aada Nwajei <aada.nwajei@gmail.com>
2024-12-06 11:32:53 +0200

    Teht. 2

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,7 +9,6 @@ class And:
 
         return True
 
-
 class PlaysIn:
     def __init__(self, team):
         self._team = team
@@ -27,3 +26,33 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+    
+class All: #tosi kaikille pelaajille
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if not matcher.test(player):
+                continue
+        return True
+
+class Not: #parametrina olevan ehdon negaatio
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return False
+            return True
+
+class HasFewerThan: #HasAtLeast-komennon negaatio eli esim. on vähemmän kuin 10 maalia
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
