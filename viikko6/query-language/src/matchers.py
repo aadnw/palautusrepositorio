d54ef910bdd12b62210055d583b82b555a7aba29
Aada Nwajei <aada.nwajei@gmail.com>
2024-12-09 15:12:59 +0200

    Teht. 4

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -31,10 +31,18 @@ class Or:
                     return True
             if matcher1.test(player):
                 return True
-            
 
         return False
+    
+class Not: #parametrina olevan ehdon negaatio
+    def __init__(self, *matchers):
+        self._matchers = matchers
 
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return False
+            return True
 
 class PlaysIn:
     def __init__(self, team):
@@ -42,7 +50,6 @@ class PlaysIn:
 
     def test(self, player):
         return player.team == self._team
-
 
 class HasAtLeast:
     def __init__(self, value, attr):
@@ -63,13 +70,19 @@ class HasFewerThan: #HasAtLeast-komennon negaatio eli esim. on vähemmän kuin 1
         player_value = getattr(player, self._attr)
 
         return player_value < self._value
-    
-class Not: #parametrina olevan ehdon negaatio
-    def __init__(self, *matchers):
-        self._matchers = matchers
 
-    def test(self, player):
-        for matcher in self._matchers:
-            if matcher.test(player):
-                return False
-            return True
+class QueryBuilder:
+    def __init__(self, build = All()):
+        self.query_build = build
+
+    def plays_in(self, team):
+        return QueryBuilder(And(PlaysIn(team), self.query_build))
+    
+    def has_at_least(self, value, attr):
+        return QueryBuilder(And(HasAtLeast(value, attr), self.query_build))
+
+    def has_fewer_than(self, value, attr):
+        return QueryBuilder(And(HasFewerThan(value, attr), self.query_build))
+    
+    def build(self):
+        return self.query_build
